2a80f544449e783292c17611f028074f0db42a73
fukuhara_mitsuaki <mfc32mobile@gmail.com>
2018-09-19 14:48:11 +0900

    UI仮

---
+++ Assets/Scripts/UIMoveUpScore.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMoveUpScore : MonoBehaviour
+{
+    public int addScore = 0;
+    Vector3 targetPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        targetPosition = GameObject.Find("TextScoreNumber").transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // update position
+        Vector3 newPos = transform.position;
+        newPos.y += 3;
+        transform.position = newPos;
+
+        if (transform.position.y > targetPosition.y - 30)
+        {
+            // update color alpha
+            Color color = GetComponent<Text>().color;
+            color.a -= 0.1f;
+            GetComponent<Text>().color = color;
+
+            if (color.a <= 0)
+            {
+                GameObject.Find("Score").GetComponent<UIScoreManager>().AddPlusScore(addScore);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UIScoreManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScoreManager : MonoBehaviour
+{
+    public GameObject moveUpObject;
+
+    GameObject scoreObject;
+    GameObject moveUpParent;
+    int score = 0;
+    int plusScore = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        scoreObject = GameObject.Find("TextScoreNumber");
+        moveUpParent = GameObject.Find("TextMoveUpParent");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            //AddPlusScore(10);
+            InstantiateMoveUpText(10);
+        }
+
+        if (plusScore > 0)
+        {
+            --plusScore;
+            ++score;
+        }
+
+        string str0 = "";
+        for (int i = 0; i < 10 - score.ToString().Length; ++i)
+        {
+            str0 += "0";
+        }
+        scoreObject.GetComponent<Text>().text = str0 + score.ToString();
+    }
+
+    public void AddPlusScore(int addScore)
+    {
+        plusScore += addScore;
+    }
+
+    public void InstantiateMoveUpText(int addScore)
+    {
+        int down = 0;
+        if (moveUpParent.transform.childCount > 0)
+        {
+            Vector3 childPos = moveUpParent.transform.GetChild(moveUpParent.transform.childCount - 1).localPosition;
+            down = (int)(childPos.y) / 40 - 2;
+        }
+        if (down > -2)
+        {
+            down = -2;
+        }
+        GameObject instObj = Instantiate<GameObject>(moveUpObject, moveUpParent.transform);
+        Vector3 instPos = instObj.transform.localPosition;
+        instPos.y = down * 40;
+        instObj.transform.localPosition = instPos;
+        instObj.GetComponent<Text>().text = "+" + addScore.ToString();
+        instObj.GetComponent<UIMoveUpScore>().addScore = addScore;
+    }
+}
