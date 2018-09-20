e129400143dd9402f84d57ff4da8c691a88868e5
fukuhara_mitsuaki <mfc32mobile@gmail.com>
2018-09-20 15:00:16 +0900

    add background.

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public GameObject garbageObject;
+    public Sprite[] sprites;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int r = Random.Range(0, 100);
+        if (r < 6)
+        {
+            GameObject obj = Instantiate(garbageObject, transform);
+            Vector3 pos = obj.transform.position;
+            pos.y += Random.Range(-5.0f, 5.0f);
+            obj.transform.position = pos;
+            obj.GetComponent<SpriteRenderer>().sprite = sprites[r];
+        }
+    }
+}
---
+++ Assets/Scripts/Garbage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Garbage : MonoBehaviour
+{
+    //Vector3 angle = new Vector3(0, 0, 0);
+    float angle = 0;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x -= 0.1f;
+        transform.position = pos;
+        angle += 0.01f;
+        transform.Rotate(new Vector3(0, 0, 1), angle);
+
+        if (pos.x < -30)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
